797a52ebf10f9c5e973e2b2881c4f4b41d8c770b
ANCB03 <ancabe1403@gmail.com>
2023-05-23 06:32:05 -0500

    Inicio del proyecto

---
+++ src/main/java/com/test_voleibol/TestVoleibolApplication.java
@@ -0,0 +1,13 @@
+package com.test_voleibol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVoleibolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestVoleibolApplication.class, args);
+    }
+
+}
