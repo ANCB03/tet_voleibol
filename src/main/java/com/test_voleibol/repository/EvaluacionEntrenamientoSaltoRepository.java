1edd67fe364dbe1c2954b88ad4ae9f57c274411c
ANCB03 <ancabe1403@gmail.com>
2023-05-23 06:52:02 -0500

    Corrección base de datos.

--- src/main/java/com/test_voleibol/repository/EvaluacionEntrenamientoSaltoRepository.java
+++ src/main/java/com/test_voleibol/repository/EvaluacionEntrenamientoSaltoRepository.java
@@ -1,6 +1,7 @@
 package com.test_voleibol.repository;
 
+import com.test_voleibol.entity.EvaluacionEntrenamientoSalto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EvaluacionEntrenamientoSaltoRepository extends JpaRepository<EvaluacionEntrenamientoSaltoRepository, Integer> {
+public interface EvaluacionEntrenamientoSaltoRepository extends JpaRepository<EvaluacionEntrenamientoSalto, Integer> {
 }
